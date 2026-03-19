c52c57b99fc1dece471ba1de92acc1a90f05eaa2
brassic88 <danilich.13.09@gmail.com>
2026-03-19 16:53:40 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,39 +1,23 @@
 import asyncio
-import logging
 from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
 from handlers import common, user, admin
 from utils.scheduler import scheduler, schedule_reminder
 from database.db import db
 
-
 async def on_startup(bot: Bot):
-    # Восстановление напоминаний при перезагрузке
-    active_bookings = db.get_all_active_bookings()
-    for booking in active_bookings:
-        # booking: (id, date, time, user_id)
-        schedule_reminder(bot, booking[3], booking[1], booking[2], booking[0])
-
+    bookings = db.get_all_active_bookings()
+    for b in bookings:
+        schedule_reminder(bot, b[3], b[1], b[2], b[0])
     scheduler.start()
 
-
 async def main():
-    logging.basicConfig(level=logging.INFO)
-
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
-
-    # Регистрация роутеров
-    dp.include_routers(common.router, user.router, admin.router)
-
-    # Запуск
-    print("Бот запущен!")
+    dp.include_routers(common.router, admin.router, user.router)
     await on_startup(bot)
+    print("Бот запущен!")
     await dp.start_polling(bot)
 
-
 if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        print("Бот выключен")
+    asyncio.run(main())
